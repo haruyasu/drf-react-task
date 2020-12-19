b24ac9f610a38a5fd82dcfe6681ba9fa801d5270
haruyasu <harukun2002@gmail.com>
2020-12-19 20:45:13 +0900

    finish django rest framework

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import TaskViewSet, CreateUserView, MyProfileView
 
 router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('myself/', MyProfileView.as_view(), name='myself'),
+    path('register/', CreateUserView.as_view(), name='register'),
 ]
