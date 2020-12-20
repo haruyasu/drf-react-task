f7d80dbaf2c1b7c84229deb148c4d2c2ed4f73ec
haruyasu <harukun2002@gmail.com>
2020-12-20 14:46:16 +0900

    TaskDetails component

---
+++ frontent/src/features/task/TaskDetails.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './TaskDetails.module.css'
+import {useSelector} from 'react-redux'
+import {selectSelectedTask} from './taskSlice'
+
+const TaskDetails = () => {
+    const selectedTask = useSelector(selectSelectedTask)
+    return (
+        <div className={styles.details}>
+            {
+                selectedTask.title && (
+                    <div>
+                        <h2>{selectedTask.title}</h2>
+                        <p>Created at</p>
+                        <h3>{selectedTask.created_at}</h3>
+                        <p>Updated at</p>
+                        <h3>{selectedTask.updated_at}</h3>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default TaskDetails
